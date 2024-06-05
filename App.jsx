9b91f7efc5b1fd143f5dbc23be5a4d70e9abaee8
Luis Angel Reyes Ventura <106917979+AngelReyesZN@users.noreply.github.com>
2024-06-04 22:20:46 -0600

    Favorites icon fixed

--- App.jsx
+++ App.jsx
@@ -1,36 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import * as SplashScreen from 'expo-splash-screen';
-import * as Font from 'expo-font';
+import React from 'react';
 import Navigation from './navigation/StackNavigator.jsx';
 
-SplashScreen.preventAutoHideAsync();
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export default function App() {
-  const [fontsLoaded, setFontsLoaded] = useState(false);
-
-  useEffect(() => {
-    async function loadFonts() {
-      await Font.loadAsync({
-        'Montserrat-Bold': require('./assets/fonts/Montserrat-Bold.ttf'),
-        'Montserrat-Medium': require('./assets/fonts/Montserrat-Medium.ttf'),
-        'Montserrat-Regular': require('./assets/fonts/Montserrat-Regular.ttf'),
-        'Montserrat-SemiBold': require('./assets/fonts/Montserrat-SemiBold.ttf'),
-      });
-      setFontsLoaded(true);
-      await SplashScreen.hideAsync();
-    }
-
-    loadFonts();
-  }, []);
-
-  if (!fontsLoaded) {
-    return null; // Puedes mostrar una pantalla de carga personalizada aquí si lo deseas
-  }
-
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <Navigation />
-    </GestureHandlerRootView>
+        <GestureHandlerRootView style={{ flex: 1 }}>
+          <Navigation />
+        </GestureHandlerRootView>
   );
 }
