63a8c6c7acb21812ca7a53ce3f8efbdcaab29e58
SlowpyWalnout <jquintana2003@gmail.com>
2024-11-06 22:22:57 -0600

    PersonalDataScreen casi lista xd
    ya casi, hago el commit por si me lo chingo xd

--- components/TopBar.jsx
+++ components/TopBar.jsx
@@ -1,20 +1,30 @@
 import React from "react";
-import { View, Image, StyleSheet, StatusBar} from "react-native";
+import { View, Image, StyleSheet, StatusBar, TouchableOpacity } from "react-native";
 import CustomText from '../components/CustomText';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { useNavigation } from '@react-navigation/native';
 
+const TopBar = ({ title, showBackButton }) => {
+  const navigation = useNavigation();
 
-const TopBar = () => {
   return (
     <View style={styles.container}>
-       <StatusBar
+      <StatusBar
         barStyle="light-content"
         backgroundColor="#FF6347"
         translucent={false}
       />
-      {/* Logo de la aplicación */}
-      <Image source={require("../assets/FoodStackLogoNT.png")} style={styles.logo} />
-      {/* Texto "CHANGARRITO FIF" */}
-      <CustomText style={styles.text} fontWeight="Medium">Food Stack</CustomText>
+      {showBackButton && (
+        <TouchableOpacity style={styles.backButton} onPress={() => navigation.goBack()}>
+          <Icon name="chevron-left" size={24} color="#FFFFFF" />
+        </TouchableOpacity>
+      )}
+      <CustomText style={styles.text} fontWeight="Medium">
+        {showBackButton ? title : 'Food Stack'}
+      </CustomText>
+      {!showBackButton && (
+        <Image source={require("../assets/FoodStackLogoNT.png")} style={styles.logo} />
+      )}
     </View>
   );
 };
@@ -27,15 +37,23 @@ const styles = StyleSheet.create({
     height: 60, // Ajusta la altura según sea necesario
     elevation: 5, // Sombra para resaltar la barra superior
     justifyContent: "center",
+    position: "relative",
+  },
+  backButton: {
+    position: "absolute",
+    left: 20,
   },
   logo: {
     width: 40,
     height: 40,
-    marginRight: 10,
+    position: "absolute",
+    right: 10,
   },
   text: {
     fontSize: 22,
     color: "#FFFFFF",
+    textAlign: "center",
+    flex: 1,
   },
 });
 
